1ec2c67feecffadb1f2f84e632ac7647060038a9
esraaabdrabo <esraaabdrabo23@gmail.com>
2022-07-27 16:25:24 +0200

    finish radio feature

--- lib/views/radio_view.dart
+++ lib/views/radio_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:just_audio/just_audio.dart';
 import 'package:new_islam/model/radio_model.dart';
 import 'package:new_islam/myThemeData.dart';
 import 'package:new_islam/view_model/radio_view_model.dart';
@@ -28,51 +29,74 @@ class Radioo extends StatelessWidget {
                         height: MediaQuery.of(context).size.height * .05,
                       ),
                       SizedBox(
-                          height: MediaQuery.of(context).size.height * .15,
-                          child: Text(radios[index].name)),
-                      SizedBox(
-                        height: MediaQuery.of(context).size.height * .05,
-                      ),
+                          height: MediaQuery.of(context).size.height * .1,
+                          width: MediaQuery.of(context).size.width * .7,
+                          child: Text(
+                            radios[index].name,
+                            textAlign: TextAlign.center,
+                            style: const TextStyle(
+                              fontSize: 20,
+                              letterSpacing: 5,
+                            ),
+                          )),
+
                       //play icon nad next
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          const Spacer(
-                            flex: 2,
-                          ),
-                          //perivous icon
-                          IconButton(
-                              onPressed: () {
-                                provider.showPerivousRadio();
-                              },
-                              icon: const Icon(
-                                Icons.arrow_left,
-                                size: 50,
-                              )),
-                          const Spacer(
-                            flex: 1,
-                          ),
-                          IconButton(
-                              onPressed: () {},
-                              icon: const Icon(
-                                Icons.play_arrow,
-                                size: 50,
-                              )),
-                          const Spacer(
-                            flex: 1,
-                          ),
-                          IconButton(
-                              onPressed: () {
-                                provider.showNextRadio();
-                              },
-                              icon: const Icon(
-                                Icons.arrow_right,
-                                size: 50,
-                              )),
-                          const Spacer(
-                            flex: 2,
-                          ),
-                        ],
+                      Container(
+                        alignment: Alignment.topCenter,
+                        height: MediaQuery.of(context).size.height * .1,
+                        width: MediaQuery.of(context).size.width * .7,
+                        padding: const EdgeInsets.all(8),
+                        decoration: BoxDecoration(
+                            color: MyThemeData.primaryColor.withAlpha(100),
+                            borderRadius:
+                                const BorderRadius.all(Radius.circular(25))),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            //perivous icon
+                            IconButton(
+                                onPressed: () {
+                                  provider.showPerivousRadio();
+                                },
+                                icon: const Icon(
+                                  Icons.arrow_left,
+                                  size: 50,
+                                )),
+
+                            //play btn
+                            !provider.isPlaying
+                                ? IconButton(
+                                    onPressed: () {
+                                      provider.play();
+                                    },
+                                    icon: const Icon(
+                                      Icons.play_arrow,
+                                      size: 50,
+                                      color: Colors.blue,
+                                    ))
+                                :
+                                //stop btn
+                                IconButton(
+                                    onPressed: () {
+                                      provider.stop();
+                                    },
+                                    icon: const Icon(
+                                      Icons.stop,
+                                      size: 50,
+                                      color: Colors.red,
+                                    )),
+
+                            //next radio
+                            IconButton(
+                                onPressed: () {
+                                  provider.showNextRadio();
+                                },
+                                icon: const Icon(
+                                  Icons.arrow_right,
+                                  size: 50,
+                                )),
+                          ],
+                        ),
                       )
                     ],
                   ),
